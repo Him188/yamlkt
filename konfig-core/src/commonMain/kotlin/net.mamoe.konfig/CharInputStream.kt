2a0787e58f9a0e5a912de610b948eb2ca9716a2d
Him188 <Him188@mamoe.net>
2020-04-03 22:47:38 +0800

    Add `CharOutputStream` implementation

--- konfig-core/src/commonMain/kotlin/net.mamoe.konfig/CharInputStream.kt
+++ konfig-core/src/commonMain/kotlin/net.mamoe.konfig/CharInputStream.kt
@@ -1,10 +1,27 @@
 package net.mamoe.konfig
 
 import kotlinx.io.core.Input
+import kotlinx.io.core.Output
 import kotlinx.io.core.readTextExactCharacters
+import kotlinx.io.core.writeText
 
 /**
- * A stream of [Char]s
+ * A stream for output [Char]s
+ */
+interface CharOutputStream {
+    /**
+     * Write the [string] to this stream
+     */
+    fun write(string: CharSequence)
+
+    /**
+     * Write the [char] to this stream
+     */
+    fun write(char: Char)
+}
+
+/**
+ * A stream for input [Char]s
  */
 interface CharInputStream {
     /**
@@ -35,6 +52,26 @@ fun Input.asCharStream(): CharInputStream = object : CharInputStream {
 
     override fun read(): Char {
         return readTextExactCharacters(1)[0]
+    }
+}
+
+fun StringBuilder.asCharStream(): CharOutputStream = object : CharOutputStream {
+    override fun write(string: CharSequence) {
+        this@asCharStream.append(string)
+    }
+
+    override fun write(char: Char) {
+        this@asCharStream.append(char)
+    }
+}
+
+fun Output.asCharStream(): CharOutputStream = object : CharOutputStream {
+    override fun write(string: CharSequence) {
+        this@asCharStream.writeText(string)
+    }
+
+    override fun write(char: Char) {
+        this@asCharStream.writeText(char.toString())
     }
 }
 
