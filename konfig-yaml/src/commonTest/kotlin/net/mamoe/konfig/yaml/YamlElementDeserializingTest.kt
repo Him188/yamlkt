ac58d2d508ce1656f10f8a05a81b907ba3c40b9b
Him188 <Him188@mamoe.net>
2020-04-26 18:05:45 +0800

    Add tests

--- konfig-yaml/src/commonTest/kotlin/net/mamoe/konfig/yaml/YamlElementDeserializingTest.kt
+++ konfig-yaml/src/commonTest/kotlin/net/mamoe/konfig/yaml/YamlElementDeserializingTest.kt
@@ -1,8 +1,11 @@
 package net.mamoe.konfig.yaml
 
 import net.mamoe.konfig.yaml.Yaml.Companion.default
+import net.mamoe.konfig.yaml.internal.YamlDecodingException
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
 
 
 internal class YamlElementDeserializingTest {
@@ -35,5 +38,96 @@ internal class YamlElementDeserializingTest {
             """.trimIndent()
             )
         )
+    }
+
+    @Test
+    fun testYamlLiteralSerializer() {
+        assertEquals(
+            YamlLiteral(
+                "banana"
+            ),
+            default.parse(
+                YamlLiteral.serializer(), """
+                banana
+            """.trimIndent()
+            )
+        )
+        assertEquals(
+            YamlLiteral(
+                "null"
+            ),
+            default.parse(
+                YamlLiteral.serializer(), """
+                "null"
+            """.trimIndent()
+            )
+        )
+        assertEquals(
+            YamlLiteral(
+                "~"
+            ),
+            default.parse(
+                YamlLiteral.serializer(), """
+                "~"
+            """.trimIndent()
+            )
+        )
+    }
+
+    @Test
+    fun testYamlLiteralSerializerWhenNull() {
+        assertTrue {
+            assertFailsWith<YamlDecodingException> {
+                default.parse(
+                    YamlLiteral.serializer(), "~"
+                )
+            }.message!!.trim().startsWith("Expected a YamlLiteral, but found YamlNull")
+        }
+
+        assertTrue {
+            assertFailsWith<YamlDecodingException> {
+                default.parse(
+                    YamlLiteral.serializer(), "null"
+                )
+            }.message!!.trim().startsWith("Expected a YamlLiteral, but found YamlNull")
+        }
+    }
+
+    @Test
+    fun testYamlNullSerializer() {
+        assertEquals(
+            YamlNull,
+            default.parse(
+                YamlNull.serializer(), "~"
+            )
+        )
+        assertEquals(
+            YamlNull,
+            default.parse(
+                YamlNull.serializer(), "null"
+            )
+        )
+
+        assertTrue {
+            assertFailsWith<YamlDecodingException> {
+                default.parse(
+                    YamlNull.serializer(), """
+                        foo
+                    """.trimIndent()
+                )
+            }.message!!.trim()
+                .also { println(it) }.startsWith("Expected a YamlNull, but found YamlLiteral(\"foo\")")
+        }
+
+        assertTrue {
+            assertFailsWith<YamlDecodingException> {
+                default.parse(
+                    YamlNull.serializer(), """
+                        "~"
+                    """.trimIndent()
+                )
+            }.message!!.trim()
+                .also { println(it) }.startsWith("Expected a YamlNull, but found YamlLiteral(\"~\")")
+        }
     }
 }
