127637010fd84553ae230eb6de7e8b74c6c6569a
Him188 <Him188@mamoe.net>
2020-04-27 15:51:42 +0800

    Fix joinTokenToString

--- konfig-yaml/src/commonTest/kotlin/net/mamoe/konfig/yaml/utils.kt
+++ konfig-yaml/src/commonTest/kotlin/net/mamoe/konfig/yaml/utils.kt
@@ -1,5 +1,6 @@
 package net.mamoe.konfig.yaml
 
+import net.mamoe.konfig.yaml.internal.END_OF_FILE
 import net.mamoe.konfig.yaml.internal.EndingTokens
 import net.mamoe.konfig.yaml.internal.Token
 import net.mamoe.konfig.yaml.internal.TokenStream
@@ -9,15 +10,16 @@ import net.mamoe.konfig.yaml.internal.TokenStream
  * Read all remaining tokens and join to string.
  */
 @Suppress("RemoveExplicitTypeArguments")
-internal fun TokenStream.joinTokenToString(): String =
-    sequence<String> {
-        while (true) {
-            yield(
-                when (val token = this@joinTokenToString.nextToken(EndingTokens.COLON)) {
-                    Token.STRING -> "String(${this@joinTokenToString.strBuff!!})"
-                    null -> return@sequence
-                    else -> token.toString()
-                }
-            )
-        }
-    }.joinToString()
+internal fun TokenStream.joinTokenToString(): String {
+    return kotlin.runCatching {
+        generateSequence {
+            when (val token = this@joinTokenToString.nextToken(EndingTokens.COLON_AND_MAP_END)) {
+                Token.STRING -> "String(${this@joinTokenToString.strBuff!!})"
+                END_OF_FILE -> null
+                else -> token.toString()
+            }
+        }.joinToString()
+    }.getOrElse {
+        throw IllegalStateException("exception on joinTokenToString", it)
+    }
+}
