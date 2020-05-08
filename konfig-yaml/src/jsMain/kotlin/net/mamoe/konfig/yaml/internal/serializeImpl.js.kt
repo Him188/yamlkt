32aa0e98b39c40401095ec078216c8381b6eac4d
Him188 <Him188@mamoe.net>
2020-05-08 15:08:20 +0800

    Improve performance

--- konfig-yaml/src/jsMain/kotlin/net/mamoe/konfig/yaml/internal/serializeImpl.js.kt
+++ konfig-yaml/src/jsMain/kotlin/net/mamoe/konfig/yaml/internal/serializeImpl.js.kt
@@ -3,6 +3,7 @@ package net.mamoe.konfig.yaml.internal
 import kotlinx.serialization.Encoder
 import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.builtins.*
 import kotlinx.serialization.serializerOrNull
 
 
@@ -12,6 +13,25 @@ internal actual fun IYamlDynamicSerializer.serializeImpl(
     encoder: Encoder,
     value: Any
 ) = when (value::class.js) {
+    Byte::class.js -> encoder.encodeSerializableValue(Byte.serializer(), value as Byte)
+    Short::class.js -> encoder.encodeSerializableValue(Short.serializer(), value as Short)
+    Int::class.js -> encoder.encodeSerializableValue(Int.serializer(), value as Int)
+    Long::class.js -> encoder.encodeSerializableValue(Long.serializer(), value as Long)
+    Float::class.js -> encoder.encodeSerializableValue(Float.serializer(), value as Float)
+    Double::class.js -> encoder.encodeSerializableValue(Double.serializer(), value as Double)
+    Char::class.js -> encoder.encodeSerializableValue(Char.serializer(), value as Char)
+    String::class.js -> encoder.encodeSerializableValue(String.serializer(), value as String)
+    Boolean::class.js -> encoder.encodeSerializableValue(Boolean.serializer(), value as Boolean)
+
+    IntArray::class.js -> encoder.encodeSerializableValue(IntArraySerializer(), value as IntArray)
+    ShortArray::class.js -> encoder.encodeSerializableValue(ShortArraySerializer(), value as ShortArray)
+    ByteArray::class.js -> encoder.encodeSerializableValue(ByteArraySerializer(), value as ByteArray)
+    LongArray::class.js -> encoder.encodeSerializableValue(LongArraySerializer(), value as LongArray)
+    CharArray::class.js -> encoder.encodeSerializableValue(CharArraySerializer(), value as CharArray)
+    FloatArray::class.js -> encoder.encodeSerializableValue(FloatArraySerializer(), value as FloatArray)
+    DoubleArray::class.js -> encoder.encodeSerializableValue(DoubleArraySerializer(), value as DoubleArray)
+    BooleanArray::class.js -> encoder.encodeSerializableValue(BooleanArraySerializer(), value as BooleanArray)
+
     Pair::class.js -> encoder.encodeSerializableValue(YamlDynamicPairSerializer, value as Pair<Any?, Any?>)
     Triple::class.js -> encoder.encodeSerializableValue(YamlDynamicTripleSerializer, value as Triple<Any?, Any?, Any?>)
 
--- konfig-yaml/src/jvmMain/kotlin/net/mamoe/konfig/yaml/internal/serializeImpl.jvm.kt
+++ konfig-yaml/src/jvmMain/kotlin/net/mamoe/konfig/yaml/internal/serializeImpl.jvm.kt
@@ -3,6 +3,7 @@ package net.mamoe.konfig.yaml.internal
 import kotlinx.serialization.Encoder
 import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.builtins.*
 import kotlinx.serialization.serializerOrNull
 
 @Suppress("UNCHECKED_CAST")
@@ -11,6 +12,25 @@ internal actual fun IYamlDynamicSerializer.serializeImpl(
     encoder: Encoder,
     value: Any
 ) = when (value::class.java) {
+    Byte::class.java -> encoder.encodeSerializableValue(Byte.serializer(), value as Byte)
+    Short::class.java -> encoder.encodeSerializableValue(Short.serializer(), value as Short)
+    Int::class.java -> encoder.encodeSerializableValue(Int.serializer(), value as Int)
+    Long::class.java -> encoder.encodeSerializableValue(Long.serializer(), value as Long)
+    Float::class.java -> encoder.encodeSerializableValue(Float.serializer(), value as Float)
+    Double::class.java -> encoder.encodeSerializableValue(Double.serializer(), value as Double)
+    Char::class.java -> encoder.encodeSerializableValue(Char.serializer(), value as Char)
+    String::class.java -> encoder.encodeSerializableValue(String.serializer(), value as String)
+    Boolean::class.java -> encoder.encodeSerializableValue(Boolean.serializer(), value as Boolean)
+
+    IntArray::class.java -> encoder.encodeSerializableValue(IntArraySerializer(), value as IntArray)
+    ShortArray::class.java -> encoder.encodeSerializableValue(ShortArraySerializer(), value as ShortArray)
+    ByteArray::class.java -> encoder.encodeSerializableValue(ByteArraySerializer(), value as ByteArray)
+    LongArray::class.java -> encoder.encodeSerializableValue(LongArraySerializer(), value as LongArray)
+    CharArray::class.java -> encoder.encodeSerializableValue(CharArraySerializer(), value as CharArray)
+    FloatArray::class.java -> encoder.encodeSerializableValue(FloatArraySerializer(), value as FloatArray)
+    DoubleArray::class.java -> encoder.encodeSerializableValue(DoubleArraySerializer(), value as DoubleArray)
+    BooleanArray::class.java -> encoder.encodeSerializableValue(BooleanArraySerializer(), value as BooleanArray)
+
     Pair::class.java -> encoder.encodeSerializableValue(YamlDynamicPairSerializer, value as Pair<Any?, Any?>)
     Triple::class.java -> encoder.encodeSerializableValue(YamlDynamicTripleSerializer, value as Triple<Any?, Any?, Any?>)
 
