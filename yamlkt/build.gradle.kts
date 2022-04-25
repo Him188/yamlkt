80c86c4fcdcfed28be3521e1213c4c521d225765
Him188 <Him188@mamoe.net>
2022-04-25 18:21:50 +0100

    Fix native platforms

--- yamlkt/build.gradle.kts
+++ yamlkt/build.gradle.kts
@@ -1,5 +1,8 @@
 @file:Suppress("UNUSED_VARIABLE")
 
+import org.apache.tools.ant.taskdefs.condition.Os
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+
 plugins {
     id("me.him188.maven-central-publish")
     kotlin("multiplatform")
@@ -21,31 +24,29 @@ kotlin {
         }
 
 
-        val ideaActive = System.getProperty("idea.active") == "true"
+        val ideaActive = System.getProperty("idea.active") == "true" && System.getProperty("publication.test") != "true"
 
-        val nativeMainSets = mutableListOf<org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet>()
-        val nativeTestSets = mutableListOf<org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet>()
+        val nativeMainSets = mutableListOf<KotlinSourceSet>()
+        val nativeTestSets = mutableListOf<KotlinSourceSet>()
 
         if (ideaActive) {
             when {
-                org.apache.tools.ant.taskdefs.condition.Os.isFamily(org.apache.tools.ant.taskdefs.condition.Os.FAMILY_MAC) -> if (org.apache.tools.ant.taskdefs.condition.Os.isArch(
-                        "aarch64"
-                    )
-                ) macosArm64("native") else macosX64("native")
-                org.apache.tools.ant.taskdefs.condition.Os.isFamily(org.apache.tools.ant.taskdefs.condition.Os.FAMILY_WINDOWS) -> mingwX64("native")
+                Os.isFamily(Os.FAMILY_MAC) -> if (Os.isArch("aarch64")) macosArm64("native") else macosX64("native")
+                Os.isFamily(Os.FAMILY_WINDOWS) -> mingwX64("native")
                 else -> linuxX64("native")
             }
         } else {
             // 1.6.0
-            val nativeTargets = arrayOf(
-                "androidNativeArm32, androidNativeArm64, androidNativeX86, androidNativeX64",
+            val nativeTargets: List<String> = arrayOf(
+                // serialization doesn't support those commented targets
+//                "androidNativeArm32, androidNativeArm64, androidNativeX86, androidNativeX64",
                 "iosArm32, iosArm64, iosX64, iosSimulatorArm64",
                 "watchosArm32, watchosArm64, watchosX86, watchosX64, watchosSimulatorArm64",
                 "tvosArm64, tvosX64, tvosSimulatorArm64",
                 "macosX64, macosArm64",
                 "linuxArm64, linuxArm32Hfp, linuxMips32, linuxMipsel32, linuxX64",
                 "mingwX64, mingwX86",
-                "wasm32"
+//                "wasm32"
             ).flatMap { it.split(", ") }
             presets.filter { it.name in nativeTargets }
                 .forEach { preset ->
@@ -53,6 +54,7 @@ kotlin {
                     nativeMainSets.add(target.compilations[org.jetbrains.kotlin.gradle.plugin.KotlinCompilation.MAIN_COMPILATION_NAME].kotlinSourceSets.first())
                     nativeTestSets.add(target.compilations[org.jetbrains.kotlin.gradle.plugin.KotlinCompilation.TEST_COMPILATION_NAME].kotlinSourceSets.first())
                 }
+
             sourceSets {
                 if (!ideaActive) {
                     configure(nativeMainSets) {
@@ -119,6 +121,14 @@ kotlin {
 
         val jsMain by getting
         val jsTest by getting
+
+        val nativeMain by getting {
+            dependsOn(commonMain)
+        }
+
+        val nativeTest by getting {
+            dependsOn(commonTest)
+        }
     }
 }
 
